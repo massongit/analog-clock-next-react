22691400fed515381f49ad0de567c1f91f480ba2
massongit <suzukimasaya428@gmail.com>
2023-08-27 20:24:34 +0900

    update-package修正

--- scripts/update_package/update_package/get_node_version.sh
+++ scripts/update_package/update_package/get_node_version.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-node_version="$(cat .node-version)"
+mapfile -t result < <(eval "${DOCKER_CMD}")
+node_version="${result[0]//v/}"
+npm_version=${result[1]}
 echo "Node.js version:" "${node_version}"
+echo "npm version:" "${npm_version}"
 echo "node_version=${node_version}" >>"${GITHUB_OUTPUT}"
+echo "npm_version=${npm_version}" >>"${GITHUB_OUTPUT}"
