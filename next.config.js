3a0919c96ad43a5a862cde321b4029e2e8343464
Masaya Suzuki <suzukimasaya428@gmail.com>
2025-02-17 10:06:39 +0900

    super-linterによる指摘事項修正

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBundleAnalyzer({});
+const nextConfig = withBundleAnalyzer({})
 
-module.exports = nextConfig;
+module.exports = nextConfig
