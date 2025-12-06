961589a552d3a477d64abb3d26eb8b40e216602f
masaya.suzuki <suzukimasaya428@gmail.com>
2025-12-07 08:20:51 +0900

    Fix by BIOME_LINT

--- src/app/clockFace/clockFace.tsx
+++ src/app/clockFace/clockFace.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { css } from "@emotion/react";
 
 export default function ClockFace({
