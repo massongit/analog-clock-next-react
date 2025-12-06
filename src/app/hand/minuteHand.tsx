3ee5a9da73cab3611473e33bedcccaa3e8919ca2
masaya.suzuki <suzukimasaya428@gmail.com>
2025-12-07 08:11:27 +0900

    Run npx @biomejs/biome format --write .

--- src/app/hand/minuteHand.tsx
+++ src/app/hand/minuteHand.tsx
@@ -3,20 +3,20 @@ import { css } from "@emotion/react";
 import Hand from "@/app/hand/hand";
 
 export default function MinuteHand({
-  clockRadius,
-  date,
+	clockRadius,
+	date,
 }: {
-  clockRadius: number;
-  date: Date;
+	clockRadius: number;
+	date: Date;
 }) {
-  return (
-    <Hand
-      clockRadius={clockRadius}
-      widthScale={0.05}
-      rotateDeg={date.getMinutes() * (360 / 60)}
-      advancedCSS={css`
+	return (
+		<Hand
+			clockRadius={clockRadius}
+			widthScale={0.05}
+			rotateDeg={date.getMinutes() * (360 / 60)}
+			advancedCSS={css`
         background-color: green;
       `}
-    />
-  );
+		/>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import React, { useEffect, useState } from "react";
 import Clock from "@/app/clock";
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setIsClient(true);
-    }
-  }, []);
-  if (isClient) {
-    return (
-      <main>
-        <Clock />
-      </main>
-    );
-  }
+	const [isClient, setIsClient] = useState(false);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setIsClient(true);
+		}
+	}, []);
+	if (isClient) {
+		return (
+			<main>
+				<Clock />
+			</main>
+		);
+	}
 }
