a63e706f830c68738501377d8741135f421bca83
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-12-10 21:54:27 +0900

    super-linterアップデート (#382)
    * Update super-linter/super-linter action to v8
    
    * gitleaksをアップデートしてあげたよ！
    
    * Set persist-credentials: false
    
    * Use flat config
    
    * Disable BIOME_FORMAT
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Clock from "@/app/clock";
 
 export default function Home() {
