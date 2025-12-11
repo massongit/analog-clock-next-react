725cab961abdcc5b1bae4bd28e5b79f524ed5924
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-12-11 09:22:43 +0900

    CI format: node_modules以下はフォーマット対象から除外する (#408)

--- scripts/format/format/format.sh
+++ scripts/format/format/format.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-find . \( -name '*.json' -o -name '*.geojson' \) -exec yq -i -o json {} \;
+find . \( -name '*.json' -o -name '*.geojson' \) -not -path './node_modules/*' -exec yq -i -o json {} \;
 find . \( -name '*.yaml' -o -name '*.yml' \) -exec yq -i {} \;
 npm run format
